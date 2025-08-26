e112c59abec8c0a520efeecdcf4f53ab5cd38806
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-08-26 10:24:49 +0600

    completed project requirement and desing with adding two extra features

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  
+
   // Use buttons to toggle between views
   document.querySelector('#submit-btn').addEventListener('click', send_email);
   document.querySelector('#emails-view').addEventListener('click', emailView);
@@ -129,7 +129,7 @@ function compose_email() {
 
 }
 
-
+// archive and unarchive email
 function toggleArchive(emailId, isArchived) {
   fetch(`/emails/${emailId}`, {
     method: 'PUT',
